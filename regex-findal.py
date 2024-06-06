2facadba6762d7ae382f987e1fdabf450fa65e72
K YASHWANTH REDDY <yashwanthkotha147@gmail.com>
2024-06-06 16:33:07 +0000

    regex-code

---
+++ regex-findal.py
@@ -0,0 +1,12 @@
+import re
+
+text = "The quick brown fox"
+pattern = r"brown"
+
+search = re.search(pattern, text)
+if search:
+    print("Pattern found:", search.group())
+else:
+    print("Pattern not found")
+
+
--- string-split.py
+++ string-split.py
@@ -1,3 +1,17 @@
 speech = "variable, of, things"
 x = speech.split(",")
 print ("split text is:", x)
+
+
+text = "Somespacesaround"
+stripped_text = text.strip()
+print("Stripped text:", stripped_text)
+
+
+str = '          1111Hello1111          '
+print(str.strip('              '))
+
+text = "Python is awesome"
+substring = "is"
+if substring in text:
+    print(substring, "found in the text")
