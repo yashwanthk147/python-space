7e2248a8ecc89ebd6392636d10d8c1fcd46c0557
K YASHWANTH REDDY <yashwanthkotha147@gmail.com>
2024-06-07 11:37:19 +0000

    funvtions in python

---
+++ functions/advanc_calc.py
@@ -0,0 +1,10 @@
+import calc as c
+x = 6
+y = 10
+
+# Call the multiplication function and print the result
+mul1 = 9
+mul2 = 10
+
+c.addition(x, y)
+c.multiplication(mul1, mul2)
---
+++ functions/calc.py
@@ -0,0 +1,39 @@
+# num1 = 4
+# num2 = 5
+
+# def addition():
+#     add = num1 + num2
+#     print(add)
+
+# def substraction():
+#     num1 = 6
+#     num2 = 8
+#     sub = num2 - num1
+#     print(sub)
+
+# def devide():
+#     dev = num1/num2
+#     print(dev)
+
+# addition()
+# substraction()
+# devide()
+
+def addition(x, y):
+
+    add = x + y
+    return add
+
+# Declare the variables outside the function
+w = 7
+z = 10
+print(addition(w, z))
+
+
+def multiplication(mul1, mul2):
+    mul = mul1 * mul2
+    return mul
+
+result = multiplication(9, 10)
+print(result)
+
