effa51f327117306833cdbb1ad9bda4f887463f0
K YASHWANTH REDDY <yashwanthkotha147@gmail.com>
2024-06-08 11:11:47 +0000

    conditions

---
+++ Operators/file.py
@@ -0,0 +1,25 @@
+def add(sum1, sum2):
+
+    sum = sum1 * sum2
+    return sum
+
+print(add(10, 9))
+
+#Comparison Operators
+
+a = 10
+b = 5
+
+lessthan = a < b
+greaterthan = a > b
+lessthanequalto = a <= b
+greaterthanequalto = a>= b
+equalto = a == b
+notequalto = a != b
+
+print("lessthan", greaterthan)
+
+#Logical Operators
+
+x = 5
+y = 4
